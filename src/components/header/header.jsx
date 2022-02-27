8f99f2ac1711865757b259ed2f1740a934d0945a
ttovely <bagievavttovely@gmail.com>
2022-02-28 00:52:27 +0600

    v3

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { CartBlock } from "../cart-block";
 import "./header.css";
 
+// import styles from './header.css';
+// { value, setValue }
 export const Header = () => {
   return (
     <div className="header">
@@ -12,10 +14,18 @@ export const Header = () => {
         </Link>
       </div>
       
+      {/* <div className={styles.search}>
+					<input value={value} onChange={e => setValue(e.target.value)} />
+					
+				</div> */}
+
         <input className="search" 
         type="text" 
         placeholder="Поиск" 
         />
+        {/* <img className="search__img">
+          <img src="/game-covers/search.png" alt="search" />
+        </img> */}
         
       <div className="wrapper header__cart-btn-wrapper">
         <CartBlock />
